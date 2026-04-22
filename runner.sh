d327921fa5a1287e282a7c8ec4d319cdf3ed0e75
Harish Bhat <hbhat@ucmerced.edu>
2026-04-22 10:24:29 -0700

    Reorganizing and adding files

---
+++ runner.sh
@@ -0,0 +1,2 @@
+python memoryFFsweep.py --dt 0.001 --mindelay 1000 --maxdelay 10000 --delaystep 100 --infile ./psi4data/fci_h2_6-31g.npz
+python memoryFFsweep.py --dt 0.001 --mindelay 500 --maxdelay 5000 --delaystep 50 --infile ./psi4data/fci_heh+_6-31g.npz
---
+++ setup.sh
@@ -0,0 +1,6 @@
+module load intel-oneapi-compilers/2025.3.1
+module load intel-oneapi-mkl/2025.3.1
+module load intel-oneapi-tbb/2022.3.0
+module load intel-oneapi-vtune/2025.3.0
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/hbhat/lib
