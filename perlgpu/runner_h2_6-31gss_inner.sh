f99953918a14cbeb6171abce97fb2539d1f41cd2
Harish S. Bhat <hbhat@ucmerced.edu>
2026-05-28 10:11:59 -0700

    adding results from perlmutter

--- perlgpu/runner_h2_6-31gss_inner.sh
+++ perlgpu/runner_h2_6-31gss_inner.sh
@@ -1,19 +1,19 @@
-# for ((delay=110;delay<=150;delay+=5)); do
-#     ./memoryFO --time 0.05,200.0 --field -1,0.1,5 --delay $delay --infile ../psi4data/fci_h2_6-31gss.npz --outpath ./ --g0 0 --g1 2 --verbose
-# done
-# for ((delay=160;delay<=200;delay+=5)); do
-#     ./memoryFO --time 0.05,200.0 --field -1,0.1,5 --delay $delay --infile ../psi4data/fci_h2_6-31gss.npz --outpath ./ --g0 0 --g1 2 --verbose
-# done
-
 for ((delay=110;delay<=150;delay+=5)); do
-    echo $delay 1 2
-    ./memoryFO --time 0.05,200.0 --field -1,0.1,5 --delay $delay --infile ../psi4data/fci_h2_6-31gss.npz --outpath ./ --g0 1 --g1 2 --verbose
+    ./memoryFO --time 0.05,200.0 --field -1,0.1,5 --delay $delay --infile ../psi4data/fci_h2_6-31gss.npz --outpath ./ --g0 0 --g1 1 --verbose
+done
+for ((delay=160;delay<=200;delay+=5)); do
+    ./memoryFO --time 0.05,200.0 --field -1,0.1,5 --delay $delay --infile ../psi4data/fci_h2_6-31gss.npz --outpath ./ --g0 0 --g1 1 --verbose
 done
 
-for ((delay=160;delay<=200;delay+=5)); do
-    echo $delay 1 2
-    ./memoryFO --time 0.05,200.0 --field -1,0.1,5 --delay $delay --infile ../psi4data/fci_h2_6-31gss.npz --outpath ./ --g0 1 --g1 2 --verbose
-done
+# for ((delay=110;delay<=150;delay+=5)); do
+#     echo $delay 1 2
+#     ./memoryFO --time 0.05,200.0 --field -1,0.1,5 --delay $delay --infile ../psi4data/fci_h2_6-31gss.npz --outpath ./ --g0 1 --g1 2 --verbose
+# done
+# 
+# for ((delay=160;delay<=200;delay+=5)); do
+#     echo $delay 1 2
+#     ./memoryFO --time 0.05,200.0 --field -1,0.1,5 --delay $delay --infile ../psi4data/fci_h2_6-31gss.npz --outpath ./ --g0 1 --g1 2 --verbose
+# done
 
 # for ((delay=110;delay<=150;delay+=5)); do
 #     echo $delay 0 3
@@ -35,12 +35,12 @@ done
 #     ./memoryFO --time 0.05,200.0 --field -1,0.1,5 --delay $delay --infile ../psi4data/fci_h2_6-31gss.npz --outpath ./ --g0 1 --g1 3 --verbose
 # done
 
-for ((delay=110;delay<=150;delay+=5)); do
-    echo $delay 2 3
-    ./memoryFO --time 0.05,200.0 --field -1,0.1,5 --delay $delay --infile ../psi4data/fci_h2_6-31gss.npz --outpath ./ --g0 2 --g1 3 --verbose
-done
-
-for ((delay=160;delay<=200;delay+=5)); do
-    echo $delay 2 3
-    ./memoryFO --time 0.05,200.0 --field -1,0.1,5 --delay $delay --infile ../psi4data/fci_h2_6-31gss.npz --outpath ./ --g0 2 --g1 3 --verbose
-done
+# for ((delay=110;delay<=150;delay+=5)); do
+#     echo $delay 2 3
+#     ./memoryFO --time 0.05,200.0 --field -1,0.1,5 --delay $delay --infile ../psi4data/fci_h2_6-31gss.npz --outpath ./ --g0 2 --g1 3 --verbose
+# done
+ 
+# for ((delay=185;delay<=200;delay+=5)); do
+#     echo $delay 2 3
+#     ./memoryFO --time 0.05,200.0 --field -1,0.1,5 --delay $delay --infile ../psi4data/fci_h2_6-31gss.npz --outpath ./ --g0 2 --g1 3 --verbose
+# done
