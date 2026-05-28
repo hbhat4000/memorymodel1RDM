db12d1a6e021c61a1ab8edd8210b6134bcfb140b
Harish Bhat <hbhat@ucmerced.edu>
2026-05-27 18:57:42 -0700

    superfine results are in

--- deltagpu/runner_h2_6-31gss_inner.sh
+++ deltagpu/runner_h2_6-31gss_inner.sh
@@ -1,11 +1,11 @@
-for ((delay=220;delay<=300;delay+=10)); do
-    ./memoryFO --time 0.025,200.0 --field -1,0.1,5 --delay $delay --infile ../psi4data/fci_h2_6-31gss.npz --outpath ./superfine/ --g0 1 --g1 2 --verbose
-done
-for ((delay=320;delay<=400;delay+=10)); do
-    ./memoryFO --time 0.025,200.0 --field -1,0.1,5 --delay $delay --infile ../psi4data/fci_h2_6-31gss.npz --outpath ./superfine/ --g0 1 --g1 2 --verbose
-done
+# for ((delay=220;delay<=300;delay+=10)); do
+#     ./memoryFO --time 0.025,200.0 --field -1,0.1,5 --delay $delay --infile ../psi4data/fci_h2_6-31gss.npz --outpath ./superfine/ --g0 1 --g1 2 --verbose
+# done
+# for ((delay=320;delay<=400;delay+=10)); do
+#     ./memoryFO --time 0.025,200.0 --field -1,0.1,5 --delay $delay --infile ../psi4data/fci_h2_6-31gss.npz --outpath ./superfine/ --g0 1 --g1 2 --verbose
+# done
 
-for ((delay=440;delay<=600;delay+=20)); do
+for ((delay=520;delay<=600;delay+=20)); do
     ./memoryFO --time 0.0125,200.0 --field -1,0.1,5 --delay $delay --infile ../psi4data/fci_h2_6-31gss.npz --outpath ./superfine/ --g0 1 --g1 2 --verbose
 done
 for ((delay=640;delay<=800;delay+=20)); do
