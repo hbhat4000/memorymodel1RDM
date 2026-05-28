3cd366d5c6a76b992e983eabd154b513fa898a7d
Harish S. Bhat <hbhat@ucmerced.edu>
2026-05-28 17:31:35 -0500

    sweep results

--- deltaoneline.sh
+++ deltaoneline.sh
@@ -1,2 +1,4 @@
 module load intel-oneapi-mkl/2024.2.2
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/u/hbhat/lib:/u/hbhat/lib64:/opt/nvidia/hpc_sdk/Linux_x86_64/25.3/math_libs/12.8/lib64
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/sw/rh9.4/spack/v1.0.0/sw/linux-x86_64_v2/intel-oneapi-mkl-2024.2.2-gkokslc/compiler/2024.2/lib
+
