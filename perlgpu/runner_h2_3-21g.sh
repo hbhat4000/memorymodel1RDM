5d663b3a06be0d244d8ccacb66d0b9ee7db4affb
Harish S. Bhat <hbhat@ucmerced.edu>
2026-05-28 12:40:07 -0700

    runners

---
+++ perlgpu/runner_h2_3-21g.sh
@@ -0,0 +1,23 @@
+for ((delay=5;delay<=955;delay+=50)); do
+    ./memoryFO --time 0.01,200.0 --field -1,0.1,5 --delay $delay --infile ../psi4data/fci_h2_3-21g.npz --outpath ./3-21g/ --g0 0 --g1 1 --verbose 
+done
+
+for ((delay=5;delay<=955;delay+=50)); do
+    ./memoryFO --time 0.01,200.0 --field -1,0.1,5 --delay $delay --infile ../psi4data/fci_h2_3-21g.npz --outpath ./3-21g/ --g0 0 --g1 2 --verbose 
+done
+
+for ((delay=5;delay<=955;delay+=50)); do
+    ./memoryFO --time 0.01,200.0 --field -1,0.1,5 --delay $delay --infile ../psi4data/fci_h2_3-21g.npz --outpath ./3-21g/ --g0 1 --g1 2 --verbose 
+done
+
+for ((delay=5;delay<=955;delay+=50)); do
+    ./memoryFO --time 0.01,200.0 --field -1,0.1,5 --delay $delay --infile ../psi4data/fci_h2_3-21g.npz --outpath ./3-21g/ --g0 0 --g1 3 --verbose 
+done
+
+for ((delay=5;delay<=955;delay+=50)); do
+    ./memoryFO --time 0.01,200.0 --field -1,0.1,5 --delay $delay --infile ../psi4data/fci_h2_3-21g.npz --outpath ./3-21g/ --g0 1 --g1 3 --verbose 
+done
+
+for ((delay=5;delay<=955;delay+=50)); do
+    ./memoryFO --time 0.01,200.0 --field -1,0.1,5 --delay $delay --infile ../psi4data/fci_h2_3-21g.npz --outpath ./3-21g/ --g0 2 --g1 3 --verbose 
+done
